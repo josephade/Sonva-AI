19bc6c1851ecaf6a289d1538e3b6272821a8381a
Destiny <237084198+destinyjeames@users.noreply.github.com>
2025-12-16 23:21:14 +0000

    Update for deployment

--- Frontend/frontend/app/components/Navbar.tsx
+++ Frontend/frontend/app/components/Navbar.tsx
@@ -94,7 +94,7 @@ const Navbar = () => {
                 transition-colors
               "
             >
-              Uses
+              Comparison
             </a>
             <a
               href="#login"
